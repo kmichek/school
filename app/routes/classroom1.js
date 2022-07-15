6ac944a901c9946411af251c32dceb51d86ea4e2
Karel <kmichek@hotmail.com>
2022-07-15 21:42:22 +0200

    Config to cookies

--- app/routes/classroom1.js
+++ app/routes/classroom1.js
@@ -1,5 +1,13 @@
 /* eslint-disable prettier/prettier */
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class Classroom1Route extends Route {
+
+  @service router;
+
+  beforeModel() {
+    this.router.transitionTo('classroom1.exercise1');
+  }
+
 }
